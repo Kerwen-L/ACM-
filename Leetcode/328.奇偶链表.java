a97680eb6771e2f0daadc0eea612e8c8a82396a9
Kerwen <1542113545@qq.com>
2020-11-13 09:41:00 +0800

    2020/11/13

---
+++ Leetcode/328.奇偶链表.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=328 lang=java
+ *
+ * [328] 奇偶链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if( head == null){
+            return null;
+        }
+        ListNode odd = head;
+        ListNode pos = head.next;
+        ListNode even = head.next;
+        while( even != null && even.next != null){
+            odd.next = even.next;
+            odd = even.next;
+            even.next = odd.next;
+            even = odd.next;
+        }
+        odd.next = pos;
+        return head;
+
+    }
+}
+// @lc code=end
+
